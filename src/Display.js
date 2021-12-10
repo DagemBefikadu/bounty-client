b2cd0ad800cd1f3343116d3ee628bad40a53bdfc
DagemBefikadu <befikadu.dagem@gmail.com>
2021-12-10 06:18:08 -0800

    Form does post

---
+++ src/Display.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Display(props) {
+    // let display 
+    // if (props.bounty === {}) {
+    //     display = (<p>Click 'more' to see more about a bounty</p>)
+    // } else {
+    //     display = (
+    //         <div className="showBounty">
+    //             <h2>{props.bounty.name}</h2>
+    //             <h3>Wanted For: {props.bounty.wantedFor}</h3>
+    //             <p>Last Seen: {props.bounty.lastSeen ? props.bounty.lastSeen: 'Unknown'}</p>
+    //         </div>
+    //     )
+    // }
+    let display 
+    if (props.bounty.name) {
+        display = (
+            <div className="showBounty">
+                <h2>{props.bounty.name}</h2>
+                <h3>Wanted For: {props.bounty.wantedFor}</h3>
+                <p>Last Seen: {props.bounty.lastSeen ? props.bounty.lastSeen: 'Unknown'}</p>
+            </div>
+        )
+    } else {
+        display = (<p>Click 'more' to see more about a bounty</p>)
+    }
+    
+    return (
+        <>
+            {display}
+        </>
+    )
+}
---
+++ src/Poster.js
@@ -0,0 +1,16 @@
+    import React from 'react'
+    
+    export default function Poster(props) {
+        let status = props.bounty.captured ? 'In Custody' : "At Large"
+
+        return (
+            <div className="Poster">
+                <h2>{status}</h2>
+                <h3>{props.bounty.name}</h3>
+                <h4>{props.bounty.reward}</h4>
+                <button onClick={() => props.changeCurrent(props.bounty)}>More</button>
+                <br />
+            </div>
+        )
+    }
+    
